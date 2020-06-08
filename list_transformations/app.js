2895ee868143cce704b176065c3c95d761fe126b
VanHieu <nvhieu.dev@gmail.com>
2020-06-08 11:29:13 +0700

    first commit

---
+++ list_transformations/app.js
@@ -0,0 +1,65 @@
+var _={}
+_.each=function(list,callback){
+    if(Array.isArray(list)){
+        //loop through array
+        for(var i=0;i<list.length;i++){
+            callback(list[i],i,list);``
+        }
+    }
+    else{
+        for(let key in list ){
+            callback(list[key],key,list);
+        }
+        // loop through obj
+    }
+}
+var x=_.each(['hieu','thao','hien'],function(name,i,list){
+    if(!list[i+1]){
+        console.log(name,"la tre nhat ");
+    }else{
+        console.log(name,'lon hon ',list[i+1])
+    }
+})
+
+var app={
+    'suspects':[
+        {
+            name:"hieu ",
+            color:"ping"
+        },
+        {
+            name:"lan",
+            color:"time"
+        }
+    ]
+}
+function createobject(name){
+    return {
+        name:name,
+        color:name.split(' ')[1],
+        speak(){
+            console.log("my name is ",name);
+        }
+    }
+};
+var sub=['mai','Hoa','lan']
+var up=[];
+_.each(sub,function(name){
+    up.push(createobject(name));
+});
+
+app['suspects'];
+
+//cai dat _.map
+_map=function(list,callback){
+    var storage=[];
+    
+    for(var i=0;i<list.length;i++){
+        storage.push(callback(list[i],i,list));
+        
+    }
+    return storage;
+}
+var tdddc=_.map([1,2,3],function(val){
+    return val+1;
+})
